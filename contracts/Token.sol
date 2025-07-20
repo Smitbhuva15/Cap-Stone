e2ee1ec15a88ee6826de423467cb3aa254391a1e
Smitbhuva15 <itssmit123@gmail.com>
2025-07-20 17:15:53 +0000

    add totalSupply and decimalgit add .

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,12 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-
 import "hardhat/console.sol";
 
-contract Token{
+contract Token {
+    //name
+    string public name;
+    //symbol
+    string public symbol;
+    // Decimal
+    uint256 public decimal = 18;
+    //Total supply
+    uint256 public totalSupply;
 
-    string constant public name="Cap Token";  
-    string constant public symbol="CAP";
-
-} 
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint256 _totalSupply
+    ) {
+        name = _name;
+        symbol = _symbol;
+        totalSupply = _totalSupply * (10 ** decimal);
+    }
+}
--- test/Tokentest.t.js
+++ test/Tokentest.t.js
@@ -1,24 +1,45 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
+const tokenCount = (n) => {
+  return ethers.utils.parseUnits(n.toString(), 'ether');
+}
 
 describe("Token", function () {
   let token;
 
   beforeEach(async () => {
     const Token = await ethers.getContractFactory("Token");
-    token=await Token.deploy();
-
-    await token.deployed();
+    token = await Token.deploy("Cap Token", "CAP", 1000000);
   })
 
-  it("has a correct name", async () => {
-    const name = await token.name();
-    expect(name).to.equal("Cap Token");
+  describe("Deployment", () => {
+     
+    const _name="Cap Token";
+    const _symbol="CAP";
+    const _totalsupply = tokenCount(1000000)
+
+    it("has a correct name", async () => {
+      const name = await token.name();
+      expect(name).to.equal(_name);
+    })
+
+    it("has a correct symbol", async () => {
+      const symbol = await token.symbol();
+      expect(symbol).to.equal(_symbol);
+    })
+
+    it("has a correct Decimal", async () => {
+      const decimal = await token.decimal();
+      expect(decimal).to.equal(18);
+    })
+
+    it("has a correct totalSupply", async () => {
+      const totalSupply = await token.totalSupply();
+      expect(totalSupply).to.equal(_totalsupply);
+
+    })
   })
 
-  it("has a correct symbol", async () => {
-    const symbol = await token.symbol();
-    expect(symbol).to.equal("CAP");
-  })
+
 })

8881ee74afe3ed130d8602d17f6fadbf149c21b8
Smitbhuva15 <itssmit123@gmail.com>
2025-07-25 17:51:56 +0000

    ui done -- buy/sell tokens

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -1,41 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Order = () => {
+    const [amount, setAmount] = useState(0);
+    const [price, setPrice] = useState(0);
+    const [isBuy, setIsBuy] = useState(true);
+
+    const handelBuy = (e) => {
+        e.preventDefault();
+        console.log("buy")
+    }
+
+    const handelSell = (e) => {
+        e.preventDefault();
+        console.log("sell")
+    }
     return (
         <div className="component exchange__orders">
             <div className='component__header flex-between'>
                 <h2>New Order</h2>
                 <div className='tabs'>
-                    <button onClick={tabHandler} ref={buyRef} className='tab tab--active'>Buy</button>
-                    <button onClick={tabHandler} ref={sellRef} className='tab'>Sell</button>
+                    <button className={`tab ${isBuy ? 'tab--active' : ""}`} onClick={() => { setIsBuy(true) }}>Buy</button>
+                    <button className={`tab ${!isBuy ? 'tab--active' : ""}`} onClick={() => { setIsBuy(false) }}>Sell</button>
                 </div>
             </div>
 
-            <form onSubmit={isBuy ? buyHandler : sellHandler}>
+            <form onSubmit={isBuy ? (e) => { handelBuy(e) } : (e) => { handelSell(e) }}>
 
-
-                <label htmlFor="amount">Buy Amount</label>
-
+                {
+                    isBuy
+                        ?
+                        (<label htmlFor="amount">Buy Amount</label>)
+                        :
+                        (<label htmlFor="amount">Sell Amount</label>)
+                }
 
 
                 <input
                     type="text"
                     id='amount'
                     placeholder='0.0000'
-
+                    value={amount === 0 ? '' : amount}
+                    onChange={(e) => { setAmount(e.target.value) }}
                 />
 
-
-                <label htmlFor="price">Buy Price</label>
+                {
+                    isBuy
+                        ?
+                        (<label htmlFor="price">Buy Price</label>)
+                        :
+                        (<label htmlFor="price">Sell Price</label>
+                        )
+                }
 
 
                 <input
                     type="text"
                     id='price'
                     placeholder='0.0000'
+                    value={price === 0 ? '' : price}
+                    onChange={(e) => { setPrice(e.target.value) }}
                 />
 
                 <button className='button button--filled' type='submit'>
+                    {
+                        isBuy
+                            ?
+                            (<span>Buy</span>)
+                            :
+                            (<span>Sell</span>)
+                    }
 
                 </button>
             </form>
